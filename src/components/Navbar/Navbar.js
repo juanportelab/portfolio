8ece7b4201a30e9530b03aa63787d696043141a7
Juan A. Portela <juanportela@cntravel.es>
2023-02-07 08:15:14 +0100

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from "./Navbar.styles";
+
+const Navbar = () => {
+  return (
+    <S.ContainerNavbar>
+      Nuestro primer container navbar
+    </S.ContainerNavbar>
+  )
+}
+
+export default Navbar

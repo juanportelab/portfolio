1f94caae62db251ed4346f9bfab175476cd2ebe3
Juan A. Portela <juanportela@cntravel.es>
2023-02-08 08:46:23 +0100

    feat: add burguer menu mobile and desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from "./Navbar.styles";
 
 const Navbar = () => {
+  const [showMenuBurger, setShowMenuBurger] = useState(false);
+
   return (
     <S.ContainerNavbar>
-      Nuestro primer container navbar
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/avatar.jpg" alt="Logo Juan Portela"></S.ImageLogo>
+        <div>
+          <h1>Juan Portela</h1>
+          <h2>Frontend developer</h2>
+        </div>
+      </S.ContainerLogo>
+        
+      <S.ContainerBurger onClick={() => setShowMenuBurger(!showMenuBurger)}>
+        <S.BurgerMenu></S.BurgerMenu>
+        <S.BurgerMenu></S.BurgerMenu>
+        <S.BurgerMenu></S.BurgerMenu>
+      </S.ContainerBurger>
+
+      <S.ContainerItems showMenuBurger={showMenuBurger}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>Contact me</S.Button>
+      </S.ContainerItems>
     </S.ContainerNavbar>
   )
 }
